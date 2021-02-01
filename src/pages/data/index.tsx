8b555263a5458872a688697f1eed8266905ff2a9
Congcong Zhang <2885546661@qq.com>
2021-02-01 10:44:11 +0800

    2021/02/01

--- src/pages/data/index.tsx
+++ src/pages/data/index.tsx
@@ -1,15 +1,15 @@
 import { View, Text } from "@tarojs/components";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { AtButton, AtCard, AtList, AtListItem } from "taro-ui";
 import dva from "../../utils/dva";
 import "./index.less";
 
 export interface personType {
-  id?: number;
+  id: number;
   name?: string;
   username?: string;
-  emile?: string;
+  email?: string;
   address?: {
     street?: string;
     suite?: string;
@@ -39,8 +39,19 @@ function mapStateToProps(state) {
 
 const DataPage: React.FC<propsType> = (props) => {
   const dispatch = dva.useDispatch();
-  const [card, setCard] = useState(false);
+  const [card, setCard] = useState([false]);
   const { dataList } = props;
+  useEffect(()=>{
+    setCard(()=>{
+      return [];
+    })
+    dataList.list.map(()=>{
+      setCard(()=>{
+        card.push(false);
+        return [...card];
+      })
+    })
+  },[dataList])
   return (
     <View className='data'>
       <AtCard title='发起请求'>
@@ -73,14 +84,23 @@ const DataPage: React.FC<propsType> = (props) => {
                 <View>
                   <AtListItem
                     title={item.username}
-                    note={item.emile}
-                    arrow='right'
+                    note={item.email}
+                    arrow={card[item.id]?'down':'right'}
                     onClick={() => {
-                      setCard(!card);
+                      
+                      setCard(()=>{
+                        card[item.id]=!card[item.id];
+                        return [...card];
+                      });
                     }}
                   />
-                  {card ? (
-                    <AtCard title={item.name} extra={item.emile}></AtCard>
+                  {card[item.id] ? (
+                    <AtCard title={item.name} note={item.website}>
+                      <View style={{float:'left',width:'150rpx'}}>street:</View><View>{item.address?.street}</View>
+                      <View style={{float:'left',width:'150rpx'}}>suite:</View><View>{item.address?.suite}</View>
+                      <View style={{float:'left',width:'150rpx'}}>city:</View><View>{item.address?.city}</View>
+                      <View style={{float:'left',width:'150rpx'}}>zipcode:</View><View>{item.address?.zipcode}</View>
+                    </AtCard>
                   ) : (
                     <View></View>
                   )}
